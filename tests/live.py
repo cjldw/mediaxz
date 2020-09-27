fd42f859712af702eb593702372b22003486d2ce
luowen <bigpao.luo@gmail.com>
2020-09-27 09:42:48 +0800

    xx

--- tests/live.py
+++ tests/live.py
@@ -12,13 +12,13 @@ import os
 class TestLive(unittest.TestCase):
 
     def test_stream(self):
-        """ffmpeg.input("C:/Users/luowen/Downloads/Video/live.mp4").output(
-            # "rtmp://yfrtmpup.cdn.zhanqi.tv/zqlive/292805_9nl9E?k=b5011c6f70ec674aa218fea8612159c6&t=5f69c003",
-            "xxx.mp4",
-            codec="copy", format="flv").run()"""
         from_url = "C:/Users/luowen/Downloads/Video/live.mp4"
         to_url = "rtmp://alrtmpup.cdn.zhanqi.tv/zqlive/292805_9nl9E?k=0fa549b60407bc4b12a94be6ba2fd8d8&t=5f69cd34"
-
-        cmd = "ffmpeg -re -i {} -codec copy -f flv {}".format(from_url, to_url)
-
-        os.system(cmd)
+        ffmpeg.input(from_url).output(
+            # "rtmp://yfrtmpup.cdn.zhanqi.tv/zqlive/292805_9nl9E?k=b5011c6f70ec674aa218fea8612159c6&t=5f69c003",
+            # "xxx.mp4",
+            to_url, codec="copy", format="flv").run()
+        #
+        # cmd = "ffmpeg -re -i {} -codec copy -f flv {}".format(from_url, to_url)
+        #
+        # os.system(cmd)
